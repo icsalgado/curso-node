151974f8715dacf573eb0ed0a86a479c6cdf3d3b
icsalgado <icsalgado@icloud.com>
2021-01-13 21:20:07 -0300

    testando express

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 //localhost
 
-app.get('/', (req, res) => res.send('Olá mundo express'))
-
-app.listen(port, () => console.log('Api rodando na porta 30000'))
+app.listen(port, () => console.log('Api rodando na porta 3000'))
