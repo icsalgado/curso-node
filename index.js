03c182e2aa580eecd5488a78138aa24f02dd659e
icsalgado <icsalgado@icloud.com>
2021-01-13 21:24:09 -0300

    Testando express no terminal e no navegador localhost:3000

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000 //localhost
 
+app.get('/', (req, res) => res.send('Olá mundo express'))
+
 app.listen(port, () => console.log('Api rodando na porta 3000'))
